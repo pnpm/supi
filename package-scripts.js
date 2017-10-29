1e36767588e5939ed9a55371b5e6c140eb6c7aff
Zoltan Kochan <zoltan.kochan@gmail.com>
2017-10-29 17:56:06 +0200

    chore: remove rimraf usage

--- package-scripts.js
+++ package-scripts.js
@@ -10,13 +10,13 @@ module.exports = {
     },
     lint: 'tslint -c tslint.json --project .',
     pnpmRegistryMock: 'pnpm-registry-mock',
-    pretest: 'rimraf ../.tmp/ && pnpm-registry-mock prepare && preview && preview',
+    pretest: 'pnpm-registry-mock prepare && preview && preview',
     test: {
       tap: 'ts-node --fast --no-cache --cache-directory ./ts-cache test',
       e2e: npsUtils.concurrent.nps('pnpm-registry-mock', 'test.tap'),
       default: 'nps pretest && nps test.e2e'
     },
-    tsc: 'rimraf lib && tsc',
+    tsc: 'tsc',
     prepublishOnly: 'npm run tsc'
   }
 }
