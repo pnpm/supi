32b457d58e1fe889bce60889c540335635ae1e9e
Zoltan Kochan <zoltan.kochan@gmail.com>
2017-10-29 12:40:23 +0200

    chore: use nps

---
+++ package-scripts.js
@@ -0,0 +1,23 @@
+'use strict'
+const npsUtils = require('nps-utils')
+
+module.exports = {
+  scripts: {
+    info: 'npm-scripts-info',
+    default: 'nps "tsc --watch"',
+    commit: {
+      description: "Run Git commit wizard",
+      script: 'commit'
+    },
+    lint: 'tslint -c tslint.json --project .',
+    pnpmRegistryMock: 'pnpm-registry-mock',
+    pretest: 'rimraf ../.tmp/ && pnpm-registry-mock prepare && preview',
+    test: {
+      tap: 'ts-node test',
+      e2e: npsUtils.concurrent.nps('pnpm-registry-mock', 'test.tap'),
+      default: 'nps pretest && nps lint && nps test.e2e'
+    },
+    tsc: 'rimraf lib && tsc',
+    prepublishOnly: 'npm run tsc'
+  }
+}
